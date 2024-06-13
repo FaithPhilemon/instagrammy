2b975d115b184c37451c902cbdfd646422d32a3a
Faith <philemonfaith@gmail.com>
2024-06-13 14:47:25 +0100

    modify main route

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Livewire\Home;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', Home::class);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
